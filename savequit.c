f99ecbfb736c4efd462144481dc5b85a2e87e2b4
Julien Zhang <jzhang52@stuy.edu>
2025-01-07 22:18:22 -0500

    testing saving and quitting

--- savequit.c
+++ savequit.c
@@ -9,35 +9,41 @@
 #include <time.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include <ncurses.h>
+#include <curses.h>
 
 int main() {
     initscr();
     cbreak();        
     noecho();          
     keypad(stdscr, TRUE); 
-    printf("Ctrl+Q - Exit\tCtrl+S - Save\n");
+    printw("Ctrl+Q - Exit\tCtrl+S - Save\n");
+    refresh();
     int c;
     while (1) {
         c = getch(); 
         if (c == 19) { // Ctrl+S
-            printf("Would you like to save your modified changes? y/n");
+            printw("Would you like to save your modified changes? y/n");
+            refresh();
             char buff[256];
             buff[1] = '\0';
             fgets(buff, 255, stdin);
             if (buff[0] == 'y' && buff[1] == '\0'){
-                printf("File name: ");
+                printw("File name: ");
+                refresh();
                 char buff1[256];
                 fgets(buff1, 255, stdin);
-                printf("File name is: %s\n", buff1);
+                printw("File name is: %s\n", buff1);
+                refresh();
             }
             else{
-                printf("Exiting...\n");
+                printw("Exiting...\n");
+                refresh();
                 break;
             } 
         }
         if (c == 17){ // Ctrl+Q
-            printf("Exiting...\n");
+            printw("Exiting...\n");
+            refresh();
             break;
         }
     }
