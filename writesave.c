7f5c05a2af98b71a15d957eb55adc1b2621f4fa6
Julien Zhang <jzhang52@stuy.edu>
2025-01-11 19:14:14 -0500

    fixing saving to a file

--- writesave.c
+++ writesave.c
@@ -12,27 +12,6 @@
 #include <ncurses.h>
 #include "filehandle.h"
 
-// Quits out of raw mode and prompts user if they want to save the changes and asks them if they want to rename the file before quitting
-void quit(struct file_buffer *file_buff) {
-  endwin();
-  printf("Would you like to save your modified changes? y/n ");
-  char buff[256];
-  buff[256] = '\0';
-  fgets(buff, 255, stdin);
-  if (buff[0] == 'y'){
-      printf("File name: ");
-      char buff1[256];
-      fgets(buff1, 255, stdin);
-      save(buff1, *file_buff);
-  }
-  else if (buff[0] == 'n'){
-      printf("Quitting...\n");
-  }
-  else{
-      printf("Invalid input. Quitting without saving...\n");
-  }
-}
-
 // Takes in the buffer and writes it into a file,
 void save(char *filename, struct file_buffer *file_buff) {
   // w+ truncatres
@@ -58,4 +37,26 @@ void save(char *filename, struct file_buffer *file_buff) {
   }
 
   close_file(file);
+}
+
+// Quits out of raw mode and prompts user if they want to save the changes and asks them if they want to rename the file before quitting
+void quit(struct file_buffer *file_buff) {
+  endwin();
+  printf("Would you like to save your modified changes? y/n ");
+  char response[256];
+  response[256] = '\0';
+  fgets(response, 255, stdin);
+  if (buff[0] == 'y'){
+      printf("File name: ");
+      char fname[256];
+      fgets(fname, 255, stdin);
+      char* fname1 = strsep(fname,"");
+      save(fname1, *file_buff);
+  }
+  else if (buff[0] == 'n'){
+      printf("Quitting...\n");
+  }
+  else{
+      printf("Invalid input. Quitting without saving...\n");
+  }
 }
