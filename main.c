1de10beed5211cf8723fb44b39ba9ff55549549d
Julien Zhang <jzhang52@stuy.edu>
2025-01-13 22:47:58 -0500

    implementing tab support

--- main.c
+++ main.c
@@ -28,6 +28,7 @@ int main(int argc, char *argv[]) {
 	int height;
 	int width;
 	int taboffset = 0;
+	int currtaboffset = 0;
 	char *filename;
 	FILE *file ;
 	char *fileinfo;
@@ -84,18 +85,25 @@ int main(int argc, char *argv[]) {
 		for (int r = 0; r < file_buff->rows; r++) {
 			wprintw(win,"%s",file_buff->buffer[r]);
 		}
-		taboffset = 0;
-		for (int i = 0; i<x; i++){
-			if ((file_buff->buffer[y-1])[i] == '\t'){
-				taboffset += 8-(taboffset%8);
+		if (currtaboffset == 0){
+			taboffset = 0
+			for (int i = 0; i<x; i++){
+				if ((file_buff->buffer[y-1])[i] == '\t'){
+					taboffset += 8-(taboffset%8);
+				}
+				else{
+					taboffset++;
+				}
 			}
-			else{
-				taboffset++;
-			}
+			taboffset-=x;
+			wmove(win, y, x+taboffset);
+			wrefresh(win);
 		}
-		taboffset-=x;
-		wmove(win, y, x+taboffset);
-		wrefresh(win);
+		else{
+			wmove(win, y, x+currtaboffset);
+			wrefresh(win);
+			currtaboffset = 0;
+		}
 		c = wgetch(win);
 		if (y == file_buff->rows){
 			xLineEnd = strlen(file_buff->buffer[y-1]);
@@ -123,6 +131,7 @@ int main(int argc, char *argv[]) {
 				if (x > strlen(file_buff->buffer[y-1])-1){
 					x = strlen(file_buff->buffer[y-1])-1;
 				}
+				currtaboffset = taboffset;
 			}
 		}
 		if (c == KEY_DOWN){
@@ -138,6 +147,7 @@ int main(int argc, char *argv[]) {
 						x = strlen(file_buff->buffer[y-1])-1;
 					}
 				}
+				currtaboffset = taboffset;
 			}
 		}
 		if (c == KEY_BACKSPACE || c == KEY_DC || c == 127){
