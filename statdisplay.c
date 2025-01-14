acd8cb6b1cf8596c26741513e9ff5f6cda346de4
David Schwartzberg <dschwartzberg50@stuy.edu>
2025-01-13 22:23:03 -0500

    add filename to stat_info string

--- statdisplay.c
+++ statdisplay.c
@@ -67,7 +67,7 @@ char* stat_info(char *path) {
 	stat(path, stat_buffer);
 	
 	char *something = (char*) calloc(LINE_SIZE, sizeof(char));
-	snprintf(something, LINE_SIZE-1, "%s bytes; last modified %s", size_to_suffix_string(stat_buffer->st_size), ctime(&(stat_buffer->st_mtime)));
+	snprintf(something, LINE_SIZE-1, "%s: %s bytes; last modified %s", path, size_to_suffix_string(stat_buffer->st_size), ctime(&(stat_buffer->st_mtime)));
 	something[LINE_SIZE-1] = '\0';
 	
 	free(stat_buffer);
