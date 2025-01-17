943ff59dcfefcc54e44184c884d507ee53131c9c
Julien Zhang <jzhang52@stuy.edu>
2025-01-16 23:41:54 -0500

    adding go to line

--- main.c
+++ main.c
@@ -174,7 +174,7 @@ int main(int argc, char *argv[]) {
 				if (c >= 48 && c <= 57){
 					sprintf(lineBuff, "%d", c1);
 					//strcat(line,&lineBuff[0]);
-					wprintw(win,"%c", lineBuff[0]);
+					wprintw(win,"%s", lineBuff);
 					wrefresh(win);
 				}
 			}
