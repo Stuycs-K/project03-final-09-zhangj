92cc0aca8760dc3f545930aeee79887641eda091
Julien Zhang <jzhang52@stuy.edu>
2025-01-16 23:33:53 -0500

    adding go to line

--- main.c
+++ main.c
@@ -37,6 +37,7 @@ int main(int argc, char *argv[]) {
 	signal(SIGSEGV, signal_handler);
 	int c, x = 0, y = 0, height, width, taboffset = 0, saved = 0, changed = 0, top = 0, lineNum, c1;
 	char *filename, *fileinfo, *line = "";
+	char lineBuff[8];
 	FILE *file;
 
 	fileinfo = (char*) calloc(LINE_SIZE, sizeof(char));
@@ -162,27 +163,26 @@ int main(int argc, char *argv[]) {
 		}
 		if (c == to_ctrl_char('G')){
 			strcpy(line,"");
-			char lineBuff[8];
 			mvwprintw(win, height-2, 0, "Go to line: ");
 			wmove(win, height-2, 12);
 			wrefresh(win);
-			// while (1){
-			// 	c1 = wgetch(win);
-			// 	if (c1 == '\n'){
-			// 		break;
-			// 	}
-			// 	if (c >= 48 && c <= 57){
-			// 		sprintf(lineBuff, "%d", c1);
-			// 		strcat(line,&lineBuff[0]);
-			// 		wprintw(win,"%c", lineBuff[0]);
-			// 	}
-			// }
-			// lineNum = atoi(line);
-			// if (lineNum < file_buff->rows){
-			// 	y = lineNum;
-			// 	curY = lineNum;
-			// 	x = strlen(file_buff->buffer[y-1]);
-			// }
+			while (1){
+				c1 = wgetch(win);
+				if (c1 == '\n'){
+					break;
+				}
+				if (c >= 48 && c <= 57){
+					sprintf(lineBuff, "%d", c1);
+					strcat(line,&lineBuff[0]);
+					wprintw(win,"%c", lineBuff[0]);
+				}
+			}
+			lineNum = atoi(line);
+			if (lineNum < file_buff->rows){
+				y = lineNum;
+				curY = lineNum;
+				x = strlen(file_buff->buffer[y-1]);
+			}
 		}
 		if (c == KEY_LEFT){
 			x = keyleft(x, y);
