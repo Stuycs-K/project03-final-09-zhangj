3a4bd97e3f852726d18dff3393774ce53ca88746
Julien Zhang <jzhang52@stuy.edu>
2025-01-18 21:20:32 -0500

    adding support for typing past window length

--- main.c
+++ main.c
@@ -256,7 +256,7 @@ int main(int argc, char *argv[]) {
 		}
 		if (32 <= c && c <= 126) { // alphanumerics, punctuation, etc.
 			changed = 1;
-			if (x+taboffset>=width-6){
+			if (x+taboffset>=width-7){
 				insert_char(file_buff,y-1,x,'-');
 				insert_char(file_buff,y-1,x+1,'\n');
 				insert_row(file_buff,y);
