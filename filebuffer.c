5815f3b10b412fb44a313ed9fd05c3edf26d89ae
Julien Zhang <jzhang52@stuy.edu>
2025-01-18 21:38:46 -0500

    adding support for typing past window length

--- filebuffer.c
+++ filebuffer.c
@@ -240,7 +240,7 @@ void read_into_buffer(FILE *file, struct file_buffer *file_buff, int winLen) {
 				resize(file_buff);
 			}
 			insert_char(file_buff,file_buff->rows-1,length,'-');
-			insert_newline(file_buff,file_buff->rows-1,winLen-8);
+			//insert_newline(file_buff,file_buff->rows-1,winLen-8);
 		}
 	}
 }
