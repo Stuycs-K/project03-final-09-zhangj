ccf9d67a0928e8e70d84cf401dbde6e081ceb665
Julien Zhang <jzhang52@stuy.edu>
2025-01-20 20:21:35 -0500

    fixing bug with file creation

--- main.c
+++ main.c
@@ -103,9 +103,11 @@ int main(int argc, char *argv[]) {
 
 	if (argc == 2){
 		read_into_buffer(file, file_buff, width);
-		int length = strlen(file_buff->buffer[file_buff->rows-1]);
-		if (file_buff->rows > 1 && file_buff->buffer[file_buff->rows-1][length-1]=='\n'){
-			insert_row(file_buff,file_buff->rows);
+		if (file_buff->rows > 1){
+			int length = strlen(file_buff->buffer[file_buff->rows-1]);
+			if (file_buff->buffer[file_buff->rows-1][length-1]=='\n'){
+				insert_row(file_buff,file_buff->rows);
+			}
 		}
 	}
 	if (argc == 1){
