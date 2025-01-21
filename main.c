d3b781ecf419975ee16a3c1597a0a3262b1fd88a
Julien Zhang <jzhang52@stuy.edu>
2025-01-20 22:45:40 -0500

    fixing bug with go to line

--- main.c
+++ main.c
@@ -290,12 +290,12 @@ int main(int argc, char *argv[]) {
 	        while (lineNum < top){
 	          top -= height-3;
 	          bottom -= height-3;
-						curY -= height-3;
+						curY += height-3;
 	        }
 	        while (lineNum > bottom){
 	          top += height-3;
 	          bottom += height-3;
-						curY += height-3;
+						curY -= height-3;
 	        }
 					x = strlen(file_buff->buffer[lineNum-1])-1;
 				}
